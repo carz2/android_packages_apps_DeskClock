b050b5d0d8d957874ecc42c846544adc0cc17b4c
Marek Walczak <mwlczk@gmail.com>
2012-11-11 18:33:48 +0100

    Add option to hide AlarmClock Icon in StatusBar
    If somebody does not need to be reminded by an icon in the status bar
    of an enabled alarm. You can switch it off.
    
    Patch Set 4: Use default preferences instead of AlarmClock.PREFERENCES
    
    Patch Set 5: Use PreferenceManager instead of static variable +
    cosmetic changes
    
    Patch Set 6: Fix strings and address code review comments
    
    Patch Set 7: Implement settings set/read per Nebkat's suggestion
    
    Patch Set 8: Fix minor nitpick
    
    Patch Set 9: Fix NPE
    
    Patch Set 12: Fix Wrong Patch Set 11
    		- Use correct DefaultPreferences instead of unused
    AlarmClock.PREFERENCES
    
    Patch Set 13: Removed obsolete null-check
    
    Change-Id: I1de4d2b084e00118d6036e05ce7dc171f73ff438

--- src/com/android/deskclock/SettingsActivity.java
+++ src/com/android/deskclock/SettingsActivity.java
@@ -17,7 +17,6 @@
 package com.android.deskclock;
 
 
-import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -27,7 +26,6 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
-import android.preference.RingtonePreference;
 import android.provider.Settings;
 
 /**
@@ -41,6 +39,8 @@ public class SettingsActivity extends PreferenceActivity
 
     private static final String KEY_ALARM_IN_SILENT_MODE =
             "alarm_in_silent_mode";
+    static final String KEY_SHOW_STATUS_BAR_ICON =
+            "show_status_bar_icon";
     static final String KEY_ALARM_SNOOZE =
             "snooze_duration";
     static final String KEY_VOLUME_BEHAVIOR =
@@ -111,6 +111,10 @@ public class SettingsActivity extends PreferenceActivity
             final ListPreference listPref = (ListPreference) pref;
             String action = (String) newValue;
             updateFlipActionSummary(listPref, action);
+        } else if (KEY_SHOW_STATUS_BAR_ICON.equals(pref.getKey())) {
+            // Check if any alarms are active. If yes and
+            // we allow showing the alarm icon, the icon will be shown.
+            Alarms.updateStatusBarIcon(getApplicationContext(), (Boolean) newValue);
         }
         return true;
     }
@@ -156,5 +160,8 @@ public class SettingsActivity extends PreferenceActivity
         String action = listPref.getValue();
         updateFlipActionSummary(listPref, action);
         listPref.setOnPreferenceChangeListener(this);
+
+        CheckBoxPreference hideStatusbarIcon = (CheckBoxPreference) findPreference(KEY_SHOW_STATUS_BAR_ICON);
+        hideStatusbarIcon.setOnPreferenceChangeListener(this);
     }
 }
